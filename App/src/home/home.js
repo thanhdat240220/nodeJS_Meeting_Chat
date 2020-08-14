d8346379ecf45a960201fc8ef068bec4ff9e561e
thanhdat240220 <thanhdat240220@gmail.com>
2020-08-14 14:34:27 +0700

    make it better 1

--- App/src/home/home.js
+++ App/src/home/home.js
@@ -106,12 +106,7 @@ const peer = new Peer(undefined, {
     config: {
         'iceServers': [
             { url: 'stun:stun.l.google.com:19302' },
-            { url: 'stun:stun1.l.google.com:19302' },
-            { url: 'stun:stun.ekiga.net' },
-            { url: 'stun:stun2.l.google.com:19302' },
-            { url: 'stun:stun3.l.google.com:19302' },
-            { url: 'stun:stun4.l.google.com:19302' },
-            { url: 'stun:stun01.sipphone.com' },
+            { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' }
         ]
     }
 });
